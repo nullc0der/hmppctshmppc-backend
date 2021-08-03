518ec4c7d5b470ef4ca4a6a961ca343041f388d0
Prasanta Kakati <prasantakakati1994@gmail.com>
2021-08-04 04:51:35 +0530

    changed backend static url

--- hmppctshmppc_backend/settings/base.py
+++ hmppctshmppc_backend/settings/base.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/public/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 # Media files
