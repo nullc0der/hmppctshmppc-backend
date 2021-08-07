2937238a13bfd00ca7f65afd5de706f0f7abcc95
Prasanta Kakati <prasantakakati1994@gmail.com>
2021-08-08 04:48:22 +0530

    [CHANGED] Enabled monero and dogecoin only

--- payment_processor/apps.py
+++ payment_processor/apps.py
@@ -2,13 +2,16 @@ from typing import Dict, Optional, Any
 from django.apps import AppConfig
 from django.conf import settings
 
-from payment_processor.daemon_api_wrapper.bitcoin import BitcoinAPIWrapper
+# from payment_processor.daemon_api_wrapper.bitcoin import BitcoinAPIWrapper
 from payment_processor.daemon_api_wrapper.dogecoin import DogeCoinAPIWrapper
 from payment_processor.daemon_api_wrapper.monero import MoneroAPIWrapper
 # from payment_processor.daemon_api_wrapper.ethereum import EthereumAPIWrapper
 
 # NOTE: In future we might modify the daemons to listen on zmq and process
-# raw tx data from there
+# raw tx data from there, also look at txnotify feature at daemons, I think
+# with this feature we can capture the hash, check the payment address, match
+# to db and update the payment, then frontend can just poll the db and check
+# its status 🎉
 
 
 class PaymentProcessorConfig(AppConfig):
@@ -19,11 +22,11 @@ class PaymentProcessorConfig(AppConfig):
         self.api_wrappers: Dict = {}
 
     def ready(self) -> None:
-        self.api_wrappers['bitcoin'] = BitcoinAPIWrapper(
-            settings.BITCOIN_DAEMON_HOST,
-            settings.BITCOIN_WALLET_RPC_USERNAME,
-            settings.BITCOIN_WALLET_RPC_PASSWORD)
-        self.api_wrappers['bitcoin'].check_wallet_loaded()
+        # self.api_wrappers['bitcoin'] = BitcoinAPIWrapper(
+        #     settings.BITCOIN_DAEMON_HOST,
+        #     settings.BITCOIN_WALLET_RPC_USERNAME,
+        #     settings.BITCOIN_WALLET_RPC_PASSWORD)
+        # self.api_wrappers['bitcoin'].check_wallet_loaded()
         self.api_wrappers['dogecoin'] = DogeCoinAPIWrapper(
             settings.DOGECOIN_DAEMON_HOST,
             settings.DOGECOIN_WALLET_RPC_USERNAME,
